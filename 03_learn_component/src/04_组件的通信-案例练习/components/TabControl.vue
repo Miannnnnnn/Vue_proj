102d2504b853f5c626c9932a421c2146e7c75498
Miannnnnnn <601268254@qq.com>
2024-11-07 15:41:46 +0800

    04_组件通信

---
+++ 03_learn_component/src/04_组件的通信-案例练习/components/TabControl.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tab-control">
+        <template v-for="item in titles" :key="item">
+            <div class="tab-control-item">{{ item }}</div>
+        </template>
+
+    </div>
+</template>
+
+
+<script>
+    export default{
+        props: {
+            titles:{
+                type: Array,
+                default: () => []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .tab-control {
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+    }
+    .tab-control-item {
+        flex: 1;
+    }
+</style>
