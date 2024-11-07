7d851db54d203ec6220a65fe62bff88d46eb4ecf
Miannnnnnn <601268254@qq.com>
2024-11-07 15:11:28 +0800

    03_组件嵌套使用_组件父子传递

---
+++ 03_learn_component/src/02_组件通信-父传子/App.vue
@@ -0,0 +1,26 @@
+<template>
+        <!-- 1.展示why的个人信息 -->
+        <!-- 如果当前的属性是一个非prop，那么该属性会默认添加到子组件的根元素上 -->
+        <show-info name="why" :age="18" :height="1.7" 
+        address="广州市" abc="cba" class="active" />
+
+        <!-- 2.展示kobe的个人信息 -->
+        <show-info name="kobe" :age="40" :height="1.98" />
+
+        <!-- 2.展示默认的个人信息 -->
+        <show-info :age="18" show-message="hahahaha"/>
+</template>
+
+
+<script>
+import ShowInfo from './ShowInfo.vue';
+    export default{
+        components: {
+            ShowInfo
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 03_learn_component/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// import App from './01_组件的嵌套使用/App.vue'
+
+import App from './03_组件通信-子传父/App.vue'
+createApp(App).mount('#app')
